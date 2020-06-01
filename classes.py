ae8d642d8c4bf0ee29af80e547dbaa2a0a89ea70
Erfan2003 <Erfan20032003@gmail.com>
2020-06-01 13:43:50 +0000

    Small change

--- classes.py
+++ classes.py
@@ -2,9 +2,6 @@ class Action:
     def __init__(self, name: str):
         self.name = name
 
-    def do(self):
-        print(f'{self.name} is done !')
-
 
 class Result:
     def __init__(self, result: str):
